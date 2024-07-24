e4ef8ab4dd6454ef93903c70d4a18421c0f32d96
Kellamendes <166321134+Kellamendes@users.noreply.github.com>
2024-07-24 11:24:05 +0530

    Create trueValues.java

---
+++ trueValues.java
@@ -0,0 +1,23 @@
+
+public class trueValues{
+	static int a;
+	static boolean b;
+	static byte c;
+	static char d;
+	static short e;
+	static long f;
+	static float g;
+	static double h;
+	public static void main(String[] args){
+		
+		
+		System.out.println("a:"+a);
+		System.out.println("b:"+b);
+		System.out.println("c:"+c);
+		System.out.println("e:"+d);
+		System.out.println("d:"+e);
+		System.out.println("f:"+f);
+		System.out.println("g:"+g);
+		System.out.println("h:"+h);
+	}
+}
